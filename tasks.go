3625b940ac83a08b78b6ef7db53158ab372889c1
call-me-maple <85463994+call-me-maple@users.noreply.github.com>
2022-06-27 21:34:38 -0500

    remove logging

--- tasks.go
+++ tasks.go
@@ -10,13 +10,11 @@ func cancelTask(key string) error {
 	value, err := client.Get(key).Result()
 	switch err {
 	case nil:
-		log.Println("task found canceling", key, value)
 		_, err = messageSend.Cancel(context.Background(), value)
 		if err != nil {
 			log.Println(err)
 		}
 	case redis.Nil:
-		log.Println("no task found", key)
 		return nil
 	default:
 		log.Println("error getting key: ", key, err)
