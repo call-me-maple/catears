06371789b4a81261d6df2304c91145944f487397
call-me-maple <85463994+call-me-maple@users.noreply.github.com>
2022-07-10 04:24:35 -0500

    use remainder instead

--- handlers.go
+++ handlers.go
@@ -81,7 +81,7 @@ func processReaction(r *bokchoy.Request) (err error) {
 	// Herb repeat check
 	case isHerbNotify(me, mr.UserID) && mr.Emoji.Name == "🔁" && !hasBotReacted(me.Reactions, "✅"):
 		err = sendHerb(&HerbOptions{
-			Stage:     1,
+			Remainder: 5,
 			ChannelID: mr.ChannelID,
 			MessageID: mr.MessageID,
 			UserID:    mr.UserID,
